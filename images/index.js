8d83ddb23dd75cab8a9a104d9f07eaf5222b8747
Arun <arunsingh641997@gmail.com>
2021-12-24 00:45:54 +0530

    1st commit

---
+++ images/index.js
@@ -0,0 +1,10 @@
+function plus(){
+    let x = +document.getElementById("count").value;
+    let y = x + 1;
+  document.getElementById("count").value = y;
+ }
+ function minus(){
+    let x = +document.getElementById("count").value;
+    let y = x - 1;
+  document.getElementById("count").value = y;
+ }
